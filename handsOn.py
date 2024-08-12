1ae279d1b9abdb11747f5061cb90cf601aac18cd
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-12 20:15:49 +0800

    ListBoxExam2Removed Code

--- handsOn.py
+++ handsOn.py
@@ -1,3 +1,4 @@
+import CTkMessagebox
 from customtkinter import *
 
 app = CTk()
@@ -5,30 +6,35 @@ app = CTk()
 app.geometry("600x400")
 app.title("Earthquake")
 
-new_list= []
+new_list = ["hey", "heyss", "heysss"]
 
-def reverse():
-    for x in range( 1, 9):
+def remove():
+    
+    try:
+        selected_text = textBox1.get("sel.first", "sel.last").strip()
+    except:
+        selected_text = ""
 
-        result = x * 3
-        new_list.append(result)
+    if selected_text == "":
+        msg = CTkMessagebox.CTkMessagebox(title="Error", message="Please select an item to remove.", icon="cancel")
+        return
 
-    new_list.reverse()
-    textBox1.configure(state="normal")
-    textBox1.insert("1.0", "\n".join(map(str, new_list))) 
+    if selected_text in new_list:
+        new_list.remove(selected_text)
 
-    textBox1.configure(state="disabled")
+        textBox1.configure(state="normal")
+        textBox1.delete("1.0", "end")
+        textBox1.insert("1.0", "\n".join(map(str, new_list)))
+        textBox1.configure(state="disabled")
+    else:
+        msg = CTkMessagebox.CTkMessagebox(title="Error", message="Selected item not found in the list.", icon="cancel")
 
-   
+textBox1 = CTkTextbox(master=app, height=120, width=100, font=("Tahoma", 12))
+textBox1.place(relx=0.3, rely=0.45)
 
+textBox1.insert("1.0", "\n".join(map(str, new_list)))
 
-
-textBox1 = CTkTextbox(master=app, height=120, width=100,font=("Tahoma",10))
-textBox1.place(relx=0.3, rely=0.4)
-
-
-
-run_btn = CTkButton(master=app, text="Run", width=75, command=reverse)
+run_btn = CTkButton(master=app, text="Remove", width=75, command=remove)
 run_btn.place(relx=0.3, rely=0.25)
 
 app.mainloop()
