32fd8dc23244de058d9b90950c8e0a1aaa4c94f7
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-12 16:16:56 +0800

    LoopExam1 code

--- handsOn.py
+++ handsOn.py
@@ -1,80 +1,35 @@
 from customtkinter import *
-import CTkMessagebox
 
 app = CTk()
 
 app.geometry("600x400")
 app.title("Earthquake")
 
-def validate_input(value):
-    if value == "" or value.isdigit():
-        return True
-    return False
+my_list = [1, 2, 3, 4, 5]
 
-def categorize_number(*args):
-    grade = grade_input.get()
+def reverse():
+    my_list.reverse()
 
-    if grade:
-        grade = float(grade)
+    textBox2.configure(state="normal")
+    textBox2.insert("1.0", "\n".join(map(str, my_list))) 
 
-        if grade <= 74:
-            result_text = "Failed"
-        elif grade > 74 and grade <= 76:
-            result_text = "3.00"
-        elif grade > 76 and grade <= 79 :
+    textBox2.configure(state="disabled")
 
-            result_text = "2.75"
 
-        elif grade > 79 and grade <= 81 :
 
-            result_text = "2.50"
 
-        elif grade > 81 and grade <= 84:
-            result_text = "2.25"
 
-        elif grade > 84 and grade <= 88 :
+textBox1 = CTkTextbox(master=app, height=100, width=80,font=("Tahoma",10))
+textBox1.place(relx=0.3, rely=0.4)
 
-            result_text = "2.00"
+textBox1.configure(state="normal")
+textBox1.insert("1.0", "\n".join(map(str, my_list))) 
+textBox1.configure(state="disabled")
 
-        elif grade > 88 and grade <= 91 :
+textBox2 = CTkTextbox(master=app, height=100, width=80,font=("Tahoma",10))
+textBox2.place(relx=0.5, rely=0.4)
 
-            result_text = "1.75"
-
-        elif grade > 91 and grade <= 94 :
-
-            result_text = "1.50"
-
-        elif grade > 94 and grade <= 98 :
-
-            result_text = "1.25"
-
-        elif grade > 98 and grade <= 100 :
-
-            result_text = "1.00"
-        else:
-            result_text = "Invalid Grade"
-
-        result.configure(state="normal")
-        result.delete(0, END)
-        result.insert(0, result_text)
-        result.configure(state="disabled")
-    else:
-        result.configure(state="normal")
-        result.delete(0, END)
-        result.configure(state="disabled")
-
-vcmd = app.register(validate_input)
-
-number_label = CTkLabel(master=app, text="Magnitude:")
-number_label.place(relx=0.28, rely=0.3)
-
-grade_input = CTkEntry(master=app, width=100, validate="key", validatecommand=(vcmd, '%P'))
-grade_input.place(relx=0.4, rely=0.3)
-grade_input.bind('<KeyRelease>', categorize_number)  
-effect_label = CTkLabel(master=app, text="Effect:")
-effect_label.place(relx=0.3, rely=0.43)
-
-result = CTkEntry(master=app, width=100, state="disabled")
-result.place(relx=0.4, rely=0.43)
+run_btn = CTkButton(master=app, text="Run", width=75, command=reverse)
+run_btn.place(relx=0.3, rely=0.25)
 
 app.mainloop()
