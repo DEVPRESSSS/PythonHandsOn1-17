fd2c2e88f87abbcb6bc11ac6d9b44d4ec7d17200
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-12 15:35:00 +0800

    Grade Code

--- handsOn.py
+++ handsOn.py
@@ -7,29 +7,52 @@ app.geometry("600x400")
 app.title("Earthquake")
 
 def validate_input(value):
-    if value == "" or value.replace('.', '', 1).isdigit():
+    if value == "" or value.isdigit():
         return True
     return False
 
 def categorize_number(*args):
-    magnitude = magnitude_input.get()
+    grade = grade_input.get()
 
-    if magnitude:
-        magnitude = float(magnitude)
+    if grade:
+        grade = float(grade)
 
-        if magnitude < 4.9:
-            result_text = "No damage"
-        elif magnitude >= 4.9 and magnitude <=5.5:
-            result_text = "Some damage"
-        elif magnitude > 5.5 and magnitude <= 6.5 :
+        if grade <= 74:
+            result_text = "Failed"
+        elif grade > 74 and grade <= 76:
+            result_text = "3.00"
+        elif grade > 76 and grade <= 79 :
 
-            result_text = "Serious damage"
+            result_text = "2.75"
 
-        elif magnitude > 6.5 and magnitude <= 7.4 :
+        elif grade > 79 and grade <= 81 :
 
-            result_text = "Disaster"
+            result_text = "2.50"
+
+        elif grade > 81 and grade <= 84:
+            result_text = "2.25"
+
+        elif grade > 84 and grade <= 88 :
+
+            result_text = "2.00"
+
+        elif grade > 88 and grade <= 91 :
+
+            result_text = "1.75"
+
+        elif grade > 91 and grade <= 94 :
+
+            result_text = "1.50"
+
+        elif grade > 94 and grade <= 98 :
+
+            result_text = "1.25"
+
+        elif grade > 98 and grade <= 100 :
+
+            result_text = "1.00"
         else:
-            result_text = "Catastrophe"
+            result_text = "Invalid Grade"
 
         result.configure(state="normal")
         result.delete(0, END)
@@ -45,9 +68,9 @@ vcmd = app.register(validate_input)
 number_label = CTkLabel(master=app, text="Magnitude:")
 number_label.place(relx=0.28, rely=0.3)
 
-magnitude_input = CTkEntry(master=app, width=100, validate="key", validatecommand=(vcmd, '%P'))
-magnitude_input.place(relx=0.4, rely=0.3)
-magnitude_input.bind('<KeyRelease>', categorize_number)  
+grade_input = CTkEntry(master=app, width=100, validate="key", validatecommand=(vcmd, '%P'))
+grade_input.place(relx=0.4, rely=0.3)
+grade_input.bind('<KeyRelease>', categorize_number)  
 effect_label = CTkLabel(master=app, text="Effect:")
 effect_label.place(relx=0.3, rely=0.43)
 
