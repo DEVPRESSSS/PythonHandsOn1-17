64fa0b1d7c5be987b2a5abc2673baa016cc0d409
devpresss xmont <xmontemorjerald@gmail.com>
2024-08-12 15:15:48 +0800

    Earthquake code

--- handsOn.py
+++ handsOn.py
@@ -4,27 +4,32 @@ import CTkMessagebox
 app = CTk()
 
 app.geometry("600x400")
-app.title("Military")
+app.title("Earthquake")
 
 def validate_input(value):
-    if value.isalpha() or value == "":
+    if value == "" or value.replace('.', '', 1).isdigit():
         return True
     return False
 
 def categorize_number(*args):
-    ship = ship_input.get()
+    magnitude = magnitude_input.get()
 
-    if ship:
-        ship = ship.lower()
+    if magnitude:
+        magnitude = float(magnitude)
 
-        if ship == "b":
-            result_text = "Battleship"
-        elif ship == "c":
-            result_text = "Cruiship"
-        elif ship == "d":
-            result_text = "Destroyer"
+        if magnitude < 4.9:
+            result_text = "No damage"
+        elif magnitude >= 4.9 and magnitude <=5.5:
+            result_text = "Some damage"
+        elif magnitude > 5.5 and magnitude <= 6.5 :
+
+            result_text = "Serious damage"
+
+        elif magnitude > 6.5 and magnitude <= 7.4 :
+
+            result_text = "Disaster"
         else:
-            result_text = "No info"
+            result_text = "Catastrophe"
 
         result.configure(state="normal")
         result.delete(0, END)
@@ -37,14 +42,14 @@ def categorize_number(*args):
 
 vcmd = app.register(validate_input)
 
-number_label = CTkLabel(master=app, text="Letter:")
-number_label.place(relx=0.3, rely=0.3)
+number_label = CTkLabel(master=app, text="Magnitude:")
+number_label.place(relx=0.28, rely=0.3)
 
-ship_input = CTkEntry(master=app, width=100, validate="key", validatecommand=(vcmd, '%P'))
-ship_input.place(relx=0.4, rely=0.3)
-ship_input.bind('<KeyRelease>', categorize_number)  
-type_label = CTkLabel(master=app, text="Type:")
-type_label.place(relx=0.3, rely=0.43)
+magnitude_input = CTkEntry(master=app, width=100, validate="key", validatecommand=(vcmd, '%P'))
+magnitude_input.place(relx=0.4, rely=0.3)
+magnitude_input.bind('<KeyRelease>', categorize_number)  
+effect_label = CTkLabel(master=app, text="Effect:")
+effect_label.place(relx=0.3, rely=0.43)
 
 result = CTkEntry(master=app, width=100, state="disabled")
 result.place(relx=0.4, rely=0.43)
